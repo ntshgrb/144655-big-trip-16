8d73a0fd7f7f6ab9a202c536d771fd304a5a0c5f
Nataly <cerise@inbox.ru>
2021-11-24 22:57:52 +0300

    Настраивает сервер для разработки

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    hot: false
+  }
 }
 
 
