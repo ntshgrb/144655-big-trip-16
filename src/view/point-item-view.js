b21a64e91711d87db15da1f711385a0081109696
Nataly <cerise@inbox.ru>
2021-12-25 14:51:26 +0300

    Реализовывает отрисовку иконок, соотвествующих типам событий

--- src/view/point-item-view.js
+++ src/view/point-item-view.js
@@ -55,7 +55,7 @@ const createPointItemTemplate = (point) => {
             <div class="event">
               <time class="event__date" datetime="${dateForattribute}">${dateForUser}</time>
               <div class="event__type">
-                <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+                <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
               </div>
               <h3 class="event__title">${type} ${destination}</h3>
               <div class="event__schedule">
